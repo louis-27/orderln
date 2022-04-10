fe7771b4969b378b82314fec2e8d2970e677ec7c
“louis-27” <louis27isnow@gmail.com>
2022-04-10 22:57:01 +0700

    feat: add driver functionalities

--- src/products/Beverages.java
+++ src/products/Beverages.java
@@ -1,23 +1,13 @@
 package products;
 
-import java.util.Random;
-
-public class Beverages extends Product implements Discountable {
+public class Beverages extends Product {
 	
-	static Random rand = new Random();
-
 	public Beverages(String name, double price) {
 		super(name, price);
 	}
 	
 	public void refill() {
 		System.out.printf("%s refilled!", getName());
-	}
-
-	@Override
-	public boolean isDiscounted() {
-		int roll = rand.nextInt(10);
-		return (roll < 4);
 	}
 
 }
---
+++ src/vehicles/Vehicle.java
@@ -0,0 +1,8 @@
+package vehicles;
+
+public abstract class Vehicle {
+
+	public String name;
+	public String licensePlate;
+
+}
